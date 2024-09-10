4ca6ce3793a3867aceed9de485cb0313770bc30d
Umakant-Mahajan <umakantmahajan9@gmaiil.com>
2024-09-10 12:52:36 -0400

    Final working app

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,17 +6,24 @@ def emotion_detector(text_to_analyse):
         input_json= { "raw_document": { "text": text_to_analyse } }
         res = requests.post(url,json=input_json, headers=Headers)
         dict_res = res.json()
+        if(res.status_code == 400):
+            emotions = {
+        'anger': "", 'disgust': "",
+        'fear':"",'joy':"", 'sadness': "" }
+            for key in emotions:
+                emotions[key]= "None"
+            emotions['dominant_emotion'] = "None"
+            return emotions
         anger_score = dict_res["emotionPredictions"][0]['emotion']['anger'] 
         disgust_score = dict_res["emotionPredictions"][0]['emotion']['disgust']
         fear_score= dict_res["emotionPredictions"][0]['emotion']['fear']
         joy_score = dict_res["emotionPredictions"][0]['emotion']['joy']
         sadness_score = dict_res["emotionPredictions"][0]['emotion']['sadness']
         emotions = {
-        'anger': anger_score,
-        'disgust': disgust_score,
-        'fear': fear_score,
-        'joy': joy_score,
-        'sadness': sadness_score}
+        'anger': anger_score, 'disgust': disgust_score,
+        'fear': fear_score,'joy': joy_score, 'sadness': sadness_score}
+        
+
         dominant_emotion = max(emotions, key=emotions.get)
         emotions['dominant_emotion'] = dominant_emotion
         return emotions
--- server.py
+++ server.py
@@ -1,27 +1,33 @@
-import requests
+'''This is the server module and handles requests to return the detected emotion '''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion_Detector")
 
-@app.route("/",methods=["GET"])
-def acces():
+
+@app.route("/", methods=["GET"])
+def access():
+    '''This is the entry point that renders the main HTML'''
     return render_template("index.html")
 
 
-@app.route("/emotionDetector", methods=["GET"])  # Fixed typo in methods
-def emotion_analysis():  # Fixed typo in function definition
+@app.route("/emotionDetector", methods=["GET"])
+def emotion_analysis():
+    '''This performs the emotion analysis on the provided text'''
     text_to_analyse = request.args.get("textToAnalyze")
     res = emotion_detector(text_to_analyse)
 
+    
+    if res["dominant_emotion"] == "None":
+        return "Invalid text! Please try again!"
+
+    
     return (
-        "For the given statement, the system response is 'anger': " + str(res["anger"]) +
-        ", 'disgust': " + str(res["disgust"]) +
-        ", 'fear': " + str(res["fear"]) +
-        ", 'joy': " + str(res["joy"]) +
-        " and 'sadness': " + str(res["sadness"]) +
-        ". The dominant emotion is " + res["dominant_emotion"] + "."
+        f"For the given statement, the system response is 'anger': {res['anger']}, "
+        f"'disgust': {res['disgust']}, 'fear': {res['fear']}, 'joy': {res['joy']}, "
+        f"and 'sadness': {res['sadness']}. The dominant emotion is {res['dominant_emotion']}."
     )
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
